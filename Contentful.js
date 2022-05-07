5cc4462edc4be6990890a332a4d942e1051fb06a
Akindele Ayorinde <AKINDELEAYORINDE55@GMAIL.COM>
2022-05-07 16:09:17 +0200

    initial decription using react

---
+++ Contentful.js
@@ -0,0 +1,8 @@
+ //import { createClient } from 'contentful'
+
+//export default createClient({
+  //space: ProcessingInstruction.env.REACT_APP_API_SPACE, 
+  //accessToken: process.env.React_APP_ACCESS_TOKEN
+  
+
+//});
